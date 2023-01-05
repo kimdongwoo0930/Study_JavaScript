e50a29313bc774544931e4f0611b1d46ddf65d2e
dongwoo <kimdongwoo0930@gmail.com>
2023-01-05 13:28:16 +0900

    자바스크립트 스터디 테스트 파일

---
+++ Lab_JavaScript/app.js
@@ -0,0 +1,24 @@
+// 형 변환
+let value = true;
+console.log(typeof value);
+
+value = String(value);
+console.log(value);
+
+console.log( "6" / "2" );
+
+let str = "123";
+let num = Number(str);
+console.log(typeof num);
+
+// 함수
+function Message(){
+    console.log("Click");
+}
+Message();
+
+function Params(param){
+    console.log(param);
+}
+Params("param");
+
